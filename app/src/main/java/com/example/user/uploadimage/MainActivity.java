b2c40946c36336d2b1948480304f724acf61e745
User <whong1996@gmail.com>
2018-01-05 15:48:41 +0800

    Firebase UploadImage

--- app/src/main/java/com/example/user/uploadimage/MainActivity.java
+++ app/src/main/java/com/example/user/uploadimage/MainActivity.java
@@ -1,13 +1,115 @@
 package com.example.user.uploadimage;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button buttonBrowse, buttonUpload;
+    ImageView imageView;
+
+    private Uri filePath;
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonBrowse = (Button) findViewById(R.id.buttonBrowse);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        buttonBrowse.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+    }
+
+    private void showFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image: "), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            //display image in ImageView
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonBrowse){
+            //open file chooser
+            showFileChooser();
+        }else if(v == buttonUpload){
+            //upload the image to firebase
+            uploadFile();
+        }
+    }
+
+    private void uploadFile() {
+        if (filePath != null) {
+            final ProgressDialog pg = new ProgressDialog(getApplicationContext());
+            pg.setTitle("Upload in progress");
+            pg.show();
+
+            StorageReference riversRef = mStorageRef.child("images/profile.jpg");
+
+            riversRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            pg.dismiss();
+                            Toast.makeText(getApplicationContext(),"Successfully uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            pg.dismiss();
+                            Toast.makeText(getApplicationContext(),"Fail to upload", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    //display percentage
+                    double p =(100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    pg.setMessage((int)p + "% uploaded");
+                }
+            });
+        }
     }
 }
